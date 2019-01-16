4bd605a13088b7661c94fdb8297c40a15b61bc83
ShahRahee <shahrahi1731@gmail.com>
2019-01-16 19:35:49 +0530

    second

--- Functional.Programs/src/Test.java
+++ Functional.Programs/src/Test.java
@@ -5,15 +5,27 @@ public class Test {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		Utility U=new Utility();
-		//U.HeadsandTails();
-		//U.Username();
-		//U.leapYear();
-		//U.powerOfTwo();
-		//U.harmonicNumber();
-		//U.primeFactors();
-		//U.Gambler(20, 25, 1000);
-		//U.Array2d();
-		//U.zeroTriplet();
+		/*
+		U.HeadsandTails();
+		U.Username();
+		U.leapYear();
+		U.powerOfTwo();
+		U.harmonicNumber();
+		U.primeFactors();
+		U.Gambler(20, 25, 1000);
+		U.Coupon();
+		U.Array2d();
+		U.zeroTriplet();
 		U.distance();
+		U.permutations();
+		U.stopWatch();
+		U.ticTacToe();
+		U.quadratic(10, 20, 20);
+		*/
+		double w=Double.parseDouble(args[0]);
+		double v=Double.parseDouble(args[1]);
+		U.windChill(w, v);
+		
+		
 	}
 }
--- Functional.Programs/src/Utility.java
+++ Functional.Programs/src/Utility.java
@@ -32,13 +32,7 @@ public class Utility {
 	}
 
 	 //genrates random number less than 1 for heads and Tails
-	float generateRandom() {
-		Random r1=new Random();
-	
-		float d= (float)r1.nextDouble();
-		return d;
-	}
-	int HeadsandTails(){
+	void HeadsandTails(){
 		/**
 		 * mHead counts number of times head was the result and not counting tails as count - head will be number of tails
 		 * mRandom is random number between 0-1. 
@@ -51,7 +45,7 @@ public class Utility {
 		float val=0.5f;
 		while(counter<num){
 			
-			mRandom=generateRandom();
+			mRandom=(float)Math.random();
 			
 			if(mRandom>val){ 
 				mHead++;
@@ -60,10 +54,8 @@ public class Utility {
 		}
 		//The result of int /int will be int hence converting 1 of them to float to get answer in decimals
 		counter=(int)(((float)mHead/counter)*100);
-		System.out.println("The Percent of head is "+counter+"and the percent of tails is "+(100-counter));
-
-		return counter;	
-		 }
+		System.out.println("The Percent of head is "+counter+" and the percent of tails is "+(100-counter));
+	 }
 	// close of Head and Tails
 	
 	Users insert(Users head,String u,String n){
@@ -255,6 +247,7 @@ public class Utility {
 			}
 			else {
 				stake--;
+				
 				System.out.println(n+" loss ");
 
 			}
@@ -263,6 +256,30 @@ public class Utility {
 		System.out.println("The number of wins are: "+win+", percentage of wins: "+(win*100/(bets)+", bets: "+bets));			
 	}
 
+	void Coupon() {
+		System.out.println("Enter the range of coupons");
+		int random,countcoupons,counttotal=0,i,range=s.nextInt();
+		int coupons[]= new int [range];
+		int []array= new int[range];
+				
+		for(i=0;i<range;i++) {
+			coupons[i]=i+1;
+			array[i]=0;
+		}
+		
+		for(countcoupons=0;countcoupons<range;counttotal++) {
+			random=(int)(Math.random()*(range+1));
+				for(i=0;i<range;i++) {
+					if(coupons[i]==random && array[i]==0) {
+						array[i]=1;
+						countcoupons++;
+					}
+				}
+		}
+		System.out.println("The number of times we tried to get all the coupons is "+counttotal);
+		
+	}
+	
 	void Array2d() throws IOException {
 		int rows,cols;
 		System.out.println("Enter the number of rows and colums");
@@ -328,11 +345,244 @@ public class Utility {
 		System.out.println("The distance from 0,0 to the current point is "+ (Math.sqrt(x*x+y*y)));
 	}
 
+	String findString(String original,String upto,int num,int []ain,int index,int length) {
+		
+		int k=1,i,j=1,flag=0,factorialleft=length-num;
+		//getting probability for the current index
+		j=j*factorialleft--; 
+		
+		while(length>num) {
+			for(k=0;k<length;k++) {
+				for(i=0;i<num;i++) {
+					if (ain[i]==k)  flag=1;
+				}
+				if(flag==0) {
+					num++;
+					upto=upto+original.charAt(k);
+					ain[num-1]=k;
+				}
+			
+			}	
+			findString(original,upto,num,ain,index,length);
+		}
+		if(length==num) {
+			System.out.println("The permutation for "+original.charAt(index)+" is "+upto);
+			num=0;
+			upto="";
+		}
+		return upto;
+	}
 	void permutations() {
-		String array="Prayas";
+		String array;
+		System.out.println("Enter the string");
+		array=s.next();
+		int i;
+		int []j= new int[array.length()];
+		for(i=0;i<array.length();i++) {
+			array=findString(array,"",0,j,i,array.length()); 
+		}	
+	}
+
+	void stopWatch() {
+		
+		/*
+		 * start stores time when prog started and stop takes the time when enter key is hitted 
+		 */
+		double start=(System.nanoTime()/1000000000),stop=0;
+		
+		System.out.println("press enter key to stop ");
+		s.hasNextLine();
+			stop=System.nanoTime()/1000000000;
+			System.out.println("Elapsed time between "+start+" and "+stop+" is "+(start-stop));
+	}
+
+	int valueAtPosition(int [][]array,int index) {
+		int i=0;
+		
+		switch(index){
+			
+		case 1: 
+			return array[0][0];
+		case 2: 
+			return array[0][1];		
+		case 3: 
+			return array[0][2];
+		case 4: 
+			return array[1][0];
+		case 5: 
+			return array[1][1];
+		case 6: 
+			return array[1][2];
+		case 7: 
+			return array[2][0];
+		case 8: 
+			return array[2][1];
+		case 9: 
+			return array[2][2];	
+		default:
+			return 1;
+		}	
+	}
+	
+	int[][] enter(int [][]array,int index,int number) {
+	
+		int i=0;
+		
+		switch(index){
+			
+		case 1: 
+			 array[0][0]=number;
+			 return array;
+		case 2: 
+			 array[0][1]=number;
+			 return array;
+		case 3: 
+			 array[0][2]=number;
+			 return array;
+		case 4: 
+			 array[1][0]=number;
+			 return array;
+		case 5: 
+			 array[1][1]=number;
+			 return array;
+		case 6: 
+			 array[1][2]=number;
+			 return array;
+		case 7: 
+			 array[2][0]=number;
+			 return array;
+		case 8: 
+			 array[2][1]=number;
+			 return array;
+		case 9: 
+			 array[2][2]=number;
+			 return array;
+		}
+		return array;
+	}
+	
+	void checkIfEnd(int a[][]) {
+		int i,j,won=1;
+		for(i=0;i<3;i++) {
+			for(j=0;j<3;j++) {
+				if(a[i][j]==0 || a[i][j]==1) continue;
+				else won=0;
+			}
+		}
+		if(won==1) {
+			System.out.println("game over");
+			return;
+		}
+	}
+	
+	int checkIfWon(int a[][],int val) {
+		int flag=0;
+		if(a[0][0]==val && ((a[0][1]==val && a[0][2]==val) || (a[1][0]==val && a[2][0]==val))){
+			flag=1;
+		}else if(a[1][1]==val && ((a[0][0]==val && a[2][2]==val)||(a[0][2]==val && a[2][0]==val))) {
+			flag =1;
+		}else if(a[1][0]==val && a[1][1]==val && a[1][2]==val) flag=1;
+		else if(a[2][0]==val && a[2][1]==val && a[2][2]==val) flag=1;
+		else if(a[0][2]==val && a[1][2]==val && a[2][2]==val) flag=1;
+		else if(a[0][1]==val && a[1][1]==val && a[2][1]==val) flag=1;
+		
+		return flag;
+	}
+
+	void printGame(int array[][]) {
+		int i,j;
+		for(i=0;i<3;i++) {
+			for(j=0;j<3;j++) {
+				
+				if(array[i][j]==0) System.out.print("X");
+				else if(array[i][j]==1) System.out.print("O");
+				else System.out.print("-");
+			}
+			System.out.println();	
+		}
+	}
+	
+	void ticTacToe(){
+		
+		//0 for user 1 for comp
+		
+		int [][]array=new int[3][3];
 		int i,j;
 		
+		for(i=0;i<3;i++) {
+			for(j=0;j<3;j++) {
+				array[i][j]=2;
+			}
+			}
+		
+		System.out.println("Your positions are marked as X");
+		int value,comp,flag=0,k;
+		
+		for(i=0;i<10;i++) {
+			
+			comp=(int)(Math.random()*10);
+			value=valueAtPosition(array,comp);
+			
+			while(value==0 || value==1) {
+				comp=(int)(Math.random()*10);
+				value=valueAtPosition(array,comp);
+			}
+			
+			array=enter(array,comp,1);
+			flag=checkIfWon(array,1);
+			if(flag==1) {
+				System.out.println("You losse");
+				printGame(array);
+				return;
+			}
+			checkIfEnd(array);
+			printGame(array);
+			System.out.println("Enter the number from 1-9 where you'd like to place the point");
+			s=new Scanner(System.in);
+			comp=s.nextInt();
+			value=valueAtPosition(array,comp);
+			while(value==0 || value==1) {
+				System.out.println("The point is already marked please enter the number");
+				s=new Scanner(System.in);
+				comp=s.nextInt();
+				value=valueAtPosition(array,comp);
+			}
+			array=enter(array,comp,0);
+			flag=checkIfWon(array,0);
+			if(flag==1) {
+				System.out.println("You Win");
+				printGame(array);
+				return;
+			}
+			checkIfEnd(array);
+
+		}
+	
+
+}
+	
+	void quadratic(int a,int b,int c) {
+		double delta,srt;
+		float rt1,rt2;
+		System.out.println("The equation is "+a+"x^2+"+b+"x+"+c+"=0");
+		
+		delta=b*b-4*a*c;
+		delta=-(delta);
+		System.out.println("delta is "+delta);
+		delta=-delta;
+		System.out.println(" square root delta is "+Math.sqrt(delta));
+		srt=Math.sqrt(delta);
+		
+		rt1=(float)((Math.sqrt(delta)-b)/2*a);
+		rt2=(float)((-Math.sqrt(delta)-b)/2*a);
+		System.out.printf("The roots of the equations are "+rt1+" and"+rt2);
+	}
+
+	void windChill(double t,double v) {
+		double w;
+		w=(0.4275*t)-35.75;
+		v=Math.pow(v,0.16);
+		System.out.println("W= "+w+" and v= "+v);
+
 	}
 }
-
-
