102c24e4b83a43bad8f78c55e81e42b7851b220f
ShahRahee <shahrahi1731@gmail.com>
2019-01-17 19:28:04 +0530

    third commit

--- Functional.Programs/src/Test.java
+++ Functional.Programs/src/Test.java
@@ -1,7 +1,12 @@
 import java.io.IOException;
 
-public class Test {
+import java.util.Scanner;
 
+
+public class Test {
+	
+	static Scanner s= new Scanner(System.in);
+	
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		Utility U=new Utility();
@@ -19,13 +24,35 @@ public class Test {
 		U.distance();
 		U.permutations();
 		U.stopWatch();
-		U.ticTacToe();
-		U.quadratic(10, 20, 20);
-		*/
-		double w=Double.parseDouble(args[0]);
+		U.windChill();
+		double t=Double.parseDouble(args[0]);
+		System.out.println("w= "+t);
 		double v=Double.parseDouble(args[1]);
-		U.windChill(w, v);
+		System.out.println("v= "+v);
 		
+		U.anagram();
+		U.anagramPrime();
+		*/
 		
-	}
+		System.out.println("Enter the size of array");
+		int j,i=s.nextInt();
+		/*
+		 * int [] array=new int[i];
+		System.out.println("Enter the array");
+		for(int j=0;j<i;j++) {
+			array[j]=s.nextInt();
+		}
+		System.out.println("Enter the number to be searched");
+		i=s.nextInt();
+		*/
+		String array[]=new String[i];
+		System.out.println("Enter the array");
+		for(j=0;j<i;j++) {
+			array[j]=s.next();
+		}
+		System.out.println("Enter the string to be searched");
+		String search=s.next();
+		Utility.BinarySearch(array,search);
+		
+		}
 }
--- Functional.Programs/src/Utility.java
+++ Functional.Programs/src/Utility.java
@@ -13,6 +13,7 @@ import java.io.PrintWriter;
 import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
+
 public class Utility {
 	
 	Scanner s=new Scanner(System.in);
@@ -66,6 +67,7 @@ public class Utility {
 		System.out.println("Enter the name");
 		n=s.next();
 		*/
+		
 		Users user=new Users(u,n);
 		if(head==null) {
 			head=user;
@@ -205,7 +207,7 @@ public class Utility {
 
 	void primeFactors() {
 		/**
-		 * counter2 is to find the factor of the number is prime
+		 * counter2 is to find the factor of the number(counter) is prime
 		 * counter is the prime factor, flag indicates the factor is prime, flag2 indicates the number is prime 
 		 */
 		int counter2=2,counter=2,flag=0,flag1=0,number;
@@ -216,6 +218,8 @@ public class Utility {
 		for(counter=2;(counter*counter)<=number;counter++) {
 			if(number%counter==0){
 				for(counter2=2;counter2<=(counter/2);counter2++) {
+					
+					//if counter is  not prime
 					if(counter%counter2==0) {
 						flag=1;
 						break;
@@ -264,11 +268,11 @@ public class Utility {
 				
 		for(i=0;i<range;i++) {
 			coupons[i]=i+1;
-			array[i]=0;
 		}
 		
 		for(countcoupons=0;countcoupons<range;counttotal++) {
 			random=(int)(Math.random()*(range+1));
+
 				for(i=0;i<range;i++) {
 					if(coupons[i]==random && array[i]==0) {
 						array[i]=1;
@@ -345,41 +349,60 @@ public class Utility {
 		System.out.println("The distance from 0,0 to the current point is "+ (Math.sqrt(x*x+y*y)));
 	}
 
-	String findString(String original,String upto,int num,int []ain,int index,int length) {
+	String findString(char c,String rest,String upto,int num,int length) {
 		
-		int k=1,i,j=1,flag=0,factorialleft=length-num;
+		int k=1,i=0,j=1,flag=0,factorialleft=length-num;
+		String temp;
 		//getting probability for the current index
 		j=j*factorialleft--; 
 		
-		while(length>num) {
+		for(i=0;num<length && i<length;i++) {
+			num++;
+	//		findString(rest.charAt(i),temp,upto,num,rest.length()); 
+			
+			
+		}
+		
+		/*
+		 * while(length>num) {
 			for(k=0;k<length;k++) {
 				for(i=0;i<num;i++) {
 					if (ain[i]==k)  flag=1;
 				}
 				if(flag==0) {
 					num++;
-					upto=upto+original.charAt(k);
+					upto=upto+rest.charAt(k);
 					ain[num-1]=k;
 				}
 			
 			}	
-			findString(original,upto,num,ain,index,length);
+			findString(rest,upto,num,ain,index,length);
 		}
+		*/
+		
+		
+		
 		if(length==num) {
-			System.out.println("The permutation for "+original.charAt(index)+" is "+upto);
+			
+			System.out.println("The permutation for "+rest.charAt(i)+" is "+upto);
 			num=0;
 			upto="";
 		}
 		return upto;
 	}
 	void permutations() {
-		String array;
+		String array,temp,curcharstring;
+		String currentchar;
 		System.out.println("Enter the string");
 		array=s.next();
+		temp=array;
 		int i;
 		int []j= new int[array.length()];
 		for(i=0;i<array.length();i++) {
-			array=findString(array,"",0,j,i,array.length()); 
+			temp= new StringBuilder(array).deleteCharAt(i).toString();
+			currentchar=Character.toString(array.charAt(i));	
+			//findString(currentchar,temp,"",0,j,i,array.length()); 
+			temp=array;
 		}	
 	}
 
@@ -397,9 +420,18 @@ public class Utility {
 	}
 
 	int valueAtPosition(int [][]array,int index) {
-		int i=0;
+		int j,i=0,num=0;
 		
-		switch(index){
+		for(i=0;i<array.length;i++) {
+			for(j=0;j<array.length;j++) {
+				num++;
+				if( num== index) { 
+					return array[i][j];
+				}
+			}	
+		}
+		
+	/*	switch(index){
 			
 		case 1: 
 			return array[0][0];
@@ -421,7 +453,9 @@ public class Utility {
 			return array[2][2];	
 		default:
 			return 1;
-		}	
+		}
+		*/	
+		return 1;
 	}
 	
 	int[][] enter(int [][]array,int index,int number) {
@@ -461,18 +495,19 @@ public class Utility {
 		return array;
 	}
 	
-	void checkIfEnd(int a[][]) {
-		int i,j,won=1;
+	int checkIfEnd(int a[][]) {
+		int i,j,flag=1;
 		for(i=0;i<3;i++) {
 			for(j=0;j<3;j++) {
 				if(a[i][j]==0 || a[i][j]==1) continue;
-				else won=0;
+				else flag=0;
 			}
 		}
-		if(won==1) {
+		if(flag==1) {
 			System.out.println("game over");
-			return;
+			return flag;
 		}
+		return flag;
 	}
 	
 	int checkIfWon(int a[][],int val) {
@@ -516,7 +551,7 @@ public class Utility {
 			}
 		
 		System.out.println("Your positions are marked as X");
-		int value,comp,flag=0,k;
+		int value,comp,flag=0;
 		
 		for(i=0;i<10;i++) {
 			
@@ -535,7 +570,11 @@ public class Utility {
 				printGame(array);
 				return;
 			}
-			checkIfEnd(array);
+			flag=checkIfEnd(array);
+			if(flag==1) {
+				printGame(array);
+				return;
+			}
 			printGame(array);
 			System.out.println("Enter the number from 1-9 where you'd like to place the point");
 			s=new Scanner(System.in);
@@ -554,35 +593,262 @@ public class Utility {
 				printGame(array);
 				return;
 			}
-			checkIfEnd(array);
-
-		}
+			flag=checkIfEnd(array);
+			if(flag==1) {
+				printGame(array);
+				return;
+			}
+		}	
 	
 
 }
 	
 	void quadratic(int a,int b,int c) {
 		double delta,srt;
+		String sign;
 		float rt1,rt2;
 		System.out.println("The equation is "+a+"x^2+"+b+"x+"+c+"=0");
 		
 		delta=b*b-4*a*c;
-		delta=-(delta);
 		System.out.println("delta is "+delta);
-		delta=-delta;
-		System.out.println(" square root delta is "+Math.sqrt(delta));
 		srt=Math.sqrt(delta);
 		
-		rt1=(float)((Math.sqrt(delta)-b)/2*a);
-		rt2=(float)((-Math.sqrt(delta)-b)/2*a);
-		System.out.printf("The roots of the equations are "+rt1+" and"+rt2);
+		if(Double.isNaN(Math.sqrt(delta))) {
+			delta=-delta;
+			srt=Math.sqrt(delta);
+			rt1=(float)(srt/2*a);
+			rt2=(float)(-srt/2*a);
+						
+			if(rt2>0) sign="-";
+			else sign="";
+			
+			System.out.printf("The roots of the equations are "+(-b/2*a)+"+"+rt1+"i and"+(-b/2*a)+sign+rt2+"i");
+		}
+		else {
+			srt=Math.sqrt(delta);
+			rt1=(float)((srt-b)/2*a);
+			rt2=(float)((-srt-b)/2*a);
+			System.out.printf("The roots of the equations are "+rt1+" and "+rt2);
+		}
 	}
-
 	void windChill(double t,double v) {
 		double w;
-		w=(0.4275*t)-35.75;
-		v=Math.pow(v,0.16);
+		//w=(0.4275*t)-35.75;
+		//v=Math.pow(v,0.16);
+		w=35.74+0.6215*t+((0.4275*t)-35.75)*(Math.pow(v,0.16));
 		System.out.println("W= "+w+" and v= "+v);
+		
+	}	
 
+	/*
+		****************Algorithms******************
+	*/
+	
+	char[] sortChar(char[] a){
+		int i,j;
+		char temp;
+		for(i=0;i<a.length;i++) {
+			for(j = i+1;j<a.length;j++) {
+					if(a[i]>a[j]){
+						temp=a[i];
+						a[i]=a[j];
+						a[j]=temp;
+					}
+			}
+		}
+		return a;
+		
 	}
+	
+	static int[] sortInt(int[] a){
+		int i,j,temp;
+		for(i=0;i<a.length;i++) {
+			for(j = i+1;j<a.length;j++) {
+					if(a[i]>a[j]){
+						temp=a[i];
+						a[i]=a[j];
+						a[j]=temp;
+					}
+			}
+		}
+		for(i=0;i<a.length;i++) System.out.print(a[i]+" ");
+		return a;
+		
+	}
+	
+	static String[] sortString(String[] a){
+		int i,j,flag;
+		String var1,var2,temp;
+		for(i=0;i<a.length;i++) {
+			for(j = i+1;j<a.length;j++) {
+				//to compare by alphabets and not by cases
+					var1=a[i].toLowerCase();
+					var2=a[j].toLowerCase();
+					flag=var1.compareTo(var2);
+					if(flag>0){
+						temp=a[i];
+						a[i]=a[j];
+						a[j]=temp;
+					}
+			}
+		}
+		for(i=0;i<a.length;i++) System.out.print(a[i]+" ");
+		return a;
+		
+	}
+	
+	void anagram(String string1, String string2){
+		int i,j,flag=0;
+		
+		if(string1.length()!=string2.length()) {
+			//System.out.println("Strings are not anagrams");
+			return;
+		}
+		
+		char aS1[]=new char[string1.length()];
+		char aS2[]=new char[string2.length()];
+		
+		for(i=0;i<string1.length();i++) {
+			aS1[i]=string1.charAt(i);
+		}
+		for(i=0;i<string2.length();i++) {
+			aS2[i]=string2.charAt(i);
+		}
+		aS1=sortChar(aS1);
+		aS2=sortChar(aS2);
+		//System.out.println(aS1);
+		//System.out.println(aS2);
+
+		for(i=0;i<string1.length();i++) {
+			if(aS1[i]!=aS2[i]) {
+				flag=1;
+				break;
+			}
+		}
+		if(flag==0) System.out.println("Strings "+string1+" and "+string2+" are anagrams");
+		
+		//else System.out.println("Strings are not anagrams");
+		
+	}
+	
+	int checkPrime(int num) {
+		int j,flag=0;
+		for(j=2;j<=num/2;j++) {
+			if(num%j==0) {
+				flag=1;
+				break;
+			}
+		}
+		
+		return flag;
+	}
+	
+	void prime() {
+		
+		int range,i,j,flag=0;
+
+		System.out.println("Enter the range");
+		range=s.nextInt();
+		if(range>2) {
+			System.out.println(" 2");
+		}
+		for(i=3;i<range;i++) {
+			flag=checkPrime(i);
+			if(flag==0) {
+				System.out.println(" "+i+"");
+				}
+			flag=0;
+		}
+	}
+	
+	void anagramPrime() {
+		
+		int range,i,j,count=0,flag=0;
+		System.out.println("Enter the range");
+		range=s.nextInt();
+		int primes[]=new int [range];
+				
+		for(i=3;i<range;i++) {
+			flag=checkPrime(i);
+			if(flag==0) {
+				primes[count]=i; //storing the prime numbers in the array
+				count++;
+				}
+			flag=0;
+		}
+		String prime1,prime2;
+		for(i=0;i<count;i++) {
+			prime1=Integer.toString(primes[i]);
+			for(j=i+1;j<count;j++) {
+				prime2=Integer.toString(primes[j]);
+				anagram(prime1,prime2);
+			}
+		}
+		for(i=0;i<count;i++) {
+			prime1=Integer.toString(primes[i]);
+			if(prime1.length()<2) continue;
+			
+			prime2=new StringBuffer(prime1).reverse().toString();
+			
+			if(prime1.equals(prime2)) {
+				System.out.println(prime1+" is a palindrome");
+			}
+		}
+	}
+
+	
+	static void BinarySearch(int array[],int num){
+			int i,length,low,high,mid;
+		array=sortInt(array);
+		length=array.length;
+		
+		low=0;
+		high=length-1;
+		
+	
+		for(i=0;i<length/2;i++) {
+			mid=(low+high)/2;
+			if(num==array[mid]) {
+				System.out.println("Number found at position "+(mid+1));
+				return;
+			}
+			if(num<array[mid]) {
+				high=mid;				
+			}
+			else {
+				low=mid;
+			}
+		}
+		System.out.println("Number not found");
+		
+		}
+	static void BinarySearch(String [] array,String num){
+			
+			array=sortString(array);
+			int i,length,low,high,mid;
+			array=sortString(array);
+			length=array.length;
+			low=0;
+			high=length-1;
+			for(i=0;i<length/2;i++) {
+				
+				mid=(low+high)/2;
+				System.out.println(mid+" "+num.equals(array[mid])+" ");
+				
+				if(num.equals(array[mid])) {
+					System.out.println("string found at position "+(mid+1));
+					return;
+				}
+				int flag=num.compareTo(array[i]);
+				if(flag>0) {
+					high=mid;		
+				}
+				else {
+					low=mid;
+				}
+			}
+			System.out.println("String not found");
+			
+		}
+	
 }
